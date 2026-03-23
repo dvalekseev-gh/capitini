812a180e5522686247b421aeb163596789f0bb8b
Dmitrii Alekseev <56812598+dvalekseev-gh@users.noreply.github.com>
2026-03-23 14:37:35 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBivILhcP8I2XjKPCc-QbDNs9sLVZgrcls",
+  authDomain: "capitini-5dfff.firebaseapp.com",
+  projectId: "capitini-5dfff",
+  storageBucket: "capitini-5dfff.firebasestorage.app",
+  messagingSenderId: "413960521320",
+  appId: "1:413960521320:web:8d61d082202a8d61a09ddd"
+};
